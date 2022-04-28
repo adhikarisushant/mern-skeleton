1e2c745122c6631fd2cb3ea18f69fa16d713443c
adhikarisushant <sushant.adhikari.619@gmail.com>
2022-04-28 15:09:25 +0545

    server setup

---
+++ config/config.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+
+const config = {
+    env: process.env.NODE_ENV || 'development',
+    port: process.env.PORT || 3000,
+    jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+    mongoUri: process.env.MONGODB_URI,
+  } 
+  
+  export default config
